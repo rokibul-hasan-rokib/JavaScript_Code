7f04ac484f7658a81fe4f8355843664dedd37c4e
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-26 13:46:06 +0600

    added in object

--- object.js
+++ object.js
@@ -1,2 +1,6 @@
 let car = {"type":"Fiat","model":"500","color":"white"};
 console.log(car.model);
+
+let car1 = {"type":"Fiat","model":"500","color":"white"};
+car1.owner = "John Doe";
+console.log(car1.owner);
