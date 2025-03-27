c5cf4fe083ac30cdf745c846d11d1ff6f1e27707
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-27 09:11:18 +0600

    object codeing done

--- object.js
+++ object.js
@@ -4,3 +4,7 @@ console.log(car.model);
 let car1 = {"type":"Fiat","model":"500","color":"white"};
 car1.owner = "John Doe";
 console.log(car1.owner);
+
+let car2 = {"type":"Fiat","model":"500","color":"white"};
+car2.owner = "John Doe";
+console.log(car2.owner);
